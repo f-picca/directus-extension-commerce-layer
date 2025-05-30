3e91e0902554861fddc931f9584bc2198c24d1a9
Fabrizio Picca <fabrizio@commercelayer.io>
2025-05-30 13:13:27 +0200

    feat: initialize Directus extension for Commerce Layer integration
    - Added package.json for extension configuration and dependencies.
    - Implemented Commerce Data Panel interface with Vue component.
    - Created endpoint for basic API response.
    - Configured TypeScript settings for the project.
    - Added Vue shims for component type definitions.

---
+++ src/commerce-layer-commerce-data-interface/interface.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="interface-box">
+	  <p>Interface Loaded 🎉 {{ commerce_layer_integration_id }} {{ commerce_layer_integration_secret }}</p>
+	</div>
+  </template>
+  
+  <script lang="ts">
+  import { defineComponent, PropType } from 'vue';
+  
+  export default defineComponent({
+	name: 'cl-commerce-data-panel',
+	props: {
+        commerce_layer_integration_id: {
+			type: String as PropType<string>,
+			required: true
+		},
+        commerce_layer_integration_secret: {
+			type: String as PropType<string>,
+			required: true
+		}
+    },
+  });
+  </script>
+  
+  <style scoped>
+  .interface-box {
+	padding: 1rem;
+	background-color: #f0f9ff;
+	border: 1px solid #cce;
+	border-radius: 6px;
+	font-size: 1.2rem;
+	text-align: center;
+	color: #333;
+  }
+  </style>
